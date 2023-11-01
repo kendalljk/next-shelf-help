51f2bf90e7e3105e67dd8d22b481a0022b274d49
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-11-01 17:38:43 -0500

    add test for functionality

--- src/app/utils/cors.js
+++ src/app/utils/cors.js
@@ -1,5 +1,5 @@
 export function setCorsHeaders(res) {
-    res.setHeader("Access-Control-Allow-Origin", `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`);
+    res.setHeader("Access-Control-Allow-Origin", `*`);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 }
